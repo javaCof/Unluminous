32f37f59f8be1a6618d2a2ce54cd4abd8ba3a71c
appler1234 <gnt741116@gmail.com>
2024-04-07 20:11:16 +0900

    플레이어스탯 구버전 제거,체스트 애니메이션

---
+++ RnD/Chest_Open/Assets/Chest/Scripts/Chest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    Animator anim;
+
+   
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void Open()
+    {
+
+    }
+
+    public void Close()
+    {
+
+    }
+}
