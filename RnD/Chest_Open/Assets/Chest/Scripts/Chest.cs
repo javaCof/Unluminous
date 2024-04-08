d10f69e5b69a0b40cd2ceb7e8de10e02d63bc3fc
appler1234 <gnt741116@gmail.com>
2024-04-08 17:58:25 +0900

    체스트 살짝 수정

--- RnD/Chest_Open/Assets/Chest/Scripts/Chest.cs
+++ RnD/Chest_Open/Assets/Chest/Scripts/Chest.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class Chest : MonoBehaviour
 {
     Animator anim;
 
-   
+    public UnityEvent ev;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+
+        if (ev!=null)
+        {
+            ev.Invoke();
+        }
     }
     public void Open()
     {
