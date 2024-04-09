067f14b7ff57ccda1f76a46d005143c09eb0f392
javaCof <javacoffee0930@gmail.com>
2024-04-09 17:29:20 +0900

    item 작업 시작

---
+++ RnD/RnD_Item/Assets/ItemObject/Scripts/ItemGenerater.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerater : MonoBehaviour
+{
+    public ItemObject item;
+
+    public void GenerateItem()
+    {
+        
+    }
+}
---
+++ RnD/RnD_Item/Assets/UnitAction/Scripts/UnitStatInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStatInfo
+{
+    public float HP;
+    public float ATK;
+    public float DEF;
+    public float SPD;
+}
