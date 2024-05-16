27b7a7d198ef502c783d4a6f01da475a1256514f
appler1234 <gnt741116@gmail.com>
2024-05-16 12:08:41 +0900

    VR플레이어 걷는 모션 추가

--- Work/DungeonOfCursed/Assets/UnitAction/Scripts/XrInput.cs
+++ Work/DungeonOfCursed/Assets/UnitAction/Scripts/XrInput.cs
@@ -19,20 +19,16 @@ public class XrInput : MonoBehaviour
 {
     [SerializeField] XRController m_left;
 
-    Animator anim;
+    [SerializeField] Animator anim;
 
+    Vector2 pos;
 
-    private void Awake()
-    {
-        gameObject.transform.GetChild(0).GetComponent<Animator>();
-
-    }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+
         IsWalk();
     }
 
@@ -40,14 +36,18 @@ public class XrInput : MonoBehaviour
     //조이스틱 값을 통해 걷는 모션조절 함수
     void IsWalk()
     {
+        if (pos == null)
+        {
+            anim.SetBool("move", false);
+        }
         //컨트롤러 조이스틱 인풋값을 pos에 넣음
-        if (m_left.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 pos))
+        else if (m_left.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out pos))
         {
             //만약 조이스틱의 상하 기울기가 0.1보다 클때
-            if (Mathf.Abs(pos.y)>0.1f)
+            if (Mathf.Abs(pos.y) > 0.1f)
             {
                 //걷는 모션
-                anim.SetBool("move",true);
+                anim.SetBool("move", true);
             }
             //0.1보다 작을때
             else if (Mathf.Abs(pos.y) > 0.1f)
@@ -57,5 +57,6 @@ public class XrInput : MonoBehaviour
             }
 
         }
+
     }
 }
