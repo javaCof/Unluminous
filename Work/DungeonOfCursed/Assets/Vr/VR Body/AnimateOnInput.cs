95a36ed24c0137f8be87b4d6d3c6df69dfb031d7
appler1234 <gnt741116@gmail.com>
2024-05-21 10:36:36 +0900

    VR플레이어 손동작 추가

--- Work/DungeonOfCursed/Assets/Vr/VR Body/AnimateOnInput.cs
+++ Work/DungeonOfCursed/Assets/Vr/VR Body/AnimateOnInput.cs
@@ -1,7 +1,61 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//using UnityEngine.XR;
+//using UnityEngine.XR.Interaction.Toolkit;
+
+
+
+//public class AnimateOnInput : MonoBehaviour
+//{
+//    float leftValue;
+//    float rightValue;
+
+//    [SerializeField] XRController m_left;
+//    [SerializeField] XRController m_right;
+
+//    public Animator anim;
+
+//    public float actionValue;
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        isGesture();
+//    }
+
+//    void isGesture()
+//    {
+//        //왼손 그립
+//        if (m_left.inputDevice.TryGetFeatureValue(CommonUsages.grip, out leftValue))
+//        {
+//            anim.SetFloat("Left Grab", leftValue);
+//        }
+
+//        //오른손 그립
+//        if (m_right.inputDevice.TryGetFeatureValue(CommonUsages.grip, out rightValue))
+//        {
+//            anim.SetFloat("Right Grab", rightValue);
+//        }
+
+//        //왼손 트리거
+//        if (m_left.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out leftValue))
+//        {
+//            anim.SetFloat("Left Pinch", leftValue);
+//        }
+
+//        //오른손 트리거
+//        if (m_right.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out rightValue))
+//        {
+//            anim.SetFloat("Right Pinch", rightValue);
+//        }
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -17,15 +71,17 @@ public class AnimateOnInput : MonoBehaviour
 
     public Animator anim;
 
-    public float actionValue;
+    Vector2 pos;
+
 
     // Update is called once per frame
     void Update()
     {
-        isGesture();
+        StartCoroutine(isGesture());
+        StartCoroutine(IsWalk());
     }
 
-    void isGesture()
+    IEnumerator isGesture()
     {
         //왼손 그립
         if (m_left.inputDevice.TryGetFeatureValue(CommonUsages.grip, out leftValue))
@@ -50,5 +106,36 @@ public class AnimateOnInput : MonoBehaviour
         {
             anim.SetFloat("Right Pinch", rightValue);
         }
+
+        yield return null;
+    }
+
+
+
+    IEnumerator IsWalk()
+    {
+
+        //컨트롤러 조이스틱 인풋값을 pos에 넣음
+        if (m_left.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out pos))
+        {
+            //만약 조이스틱의 상하 기울기가 0.1보다 클때
+            if (Mathf.Abs(pos.y) > 0.1f)
+            {
+                //걷는 모션
+                anim.SetBool("move", true);
+
+
+            }
+            //0.1보다 작을때
+            else if (Mathf.Abs(pos.y) < 0.1f)
+            {
+
+                //가만히 모션
+                anim.SetBool("move", false);
+
+            }
+        }
+
+        yield return null;
     }
 }
